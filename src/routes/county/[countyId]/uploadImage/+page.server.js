b8137b626150c3af624a9edeb12f0dc00446e273
Vinod-Yadav <bkvinodyadav@hotmail.com>
2023-09-03 18:26:45 +0100

    netlify image bug fix 8

--- src/routes/county/[countyId]/uploadImage/+page.server.js
+++ src/routes/county/[countyId]/uploadImage/+page.server.js
@@ -3,7 +3,7 @@ import { redirect } from "@sveltejs/kit";
 import { universityService } from "../../../../services/university-service";
 // export const ssr=false;
 
-let formData = null;
+
 
 
 export const actions = {
@@ -12,18 +12,9 @@ export const actions = {
     const countyId = await params.countyId;
     console.log(`attempting to upload image with id ${countyId}`);
     await universityService.uploadImage(data,countyId);
-    if (data) {
-      formData = true
-    }
     throw redirect(303,`/county/${params.countyId}`);
   }
 }
 
-export async function load({request, params})  {
-  //universityService.reload();
-  const countyId = await params.countyId;
-  //countyStore.set(county);
-  //console.log(get(countyStore));
-  return {countyId,formData}
-}
+
 
--- src/routes/county/[countyId]/uploadImage/+page.svelte
+++ src/routes/county/[countyId]/uploadImage/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-  export let data;
-
-  console.log(data.countyId);
-  console.log(data.formData)
-
-</script>
 
 <p>Image uploaded</p>
 
